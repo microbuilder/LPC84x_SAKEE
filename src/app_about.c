63c790ca18e72c1000e88a4209a12d4fab2d3083
microbuilder <contact@microbuilder.eu>
2017-09-12 07:30:55 +0200

    Added version number to about screen

--- src/app_about.c
+++ src/app_about.c
@@ -30,8 +30,15 @@ void app_about_run(void)
     ssd1306_set_text(0, 0, 1, "NXP SAKEE", 1);
     ssd1306_set_text(127-60, 0, 1, "ABOUT SAKEE", 1);
 	ssd1306_set_text(2, 16, 1, "LPC845 SAKEE", 2);
-	ssd1306_set_text(2, 32, 1, "(C) NXP 2017", 2);
+	//ssd1306_set_text(2, 32, 1, VERSION_MAJOR "." VERSION_MINOR "." VERSION_REVISION, 2);
 	ssd1306_set_text(16, 55, 1, "CLICK FOR MAIN MENU", 1);
+
+	/* Revision string */
+	gfx_printdec(34, 32, VERSION_MAJOR, 2, 1);
+	ssd1306_set_text(46, 32, 1, ".", 2);
+	gfx_printdec(58, 32, VERSION_MINOR, 2, 1);
+	ssd1306_set_text(70, 32, 1, ".", 2);
+	gfx_printdec(82, 32, VERSION_REVISION, 2, 1);
 
     ssd1306_refresh();
 
